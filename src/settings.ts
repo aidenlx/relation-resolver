709cbf162b22a974a2fb32501c5136f360e56a0d
AidenLx <aiden.lx@outlook.com>
2021-08-10 17:18:56 +0800

    feat(settings): add options for children/parent field setup

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-import { App, PluginSettingTab, Setting } from "obsidian";
+import { App, debounce, PluginSettingTab, Setting } from "obsidian";
 import RelationResolver from "rr-main";
 
 import { RelationInField } from "./api";
@@ -26,5 +26,34 @@ export class RelationResolverSettingTab extends PluginSettingTab {
   display(): void {
     const { containerEl } = this;
     containerEl.empty();
+
+    this.setFieldNames();
+  }
+
+  setFieldNames() {
+    const { settings } = this.plugin;
+    const firstUpperCase = ([first, ...rest]: string) =>
+      first?.toUpperCase() + rest.join("");
+    const setup = (field: keyof RelationResolverSettings["fieldNames"]) =>
+      new Setting(this.containerEl)
+        .setName(`${firstUpperCase(field)} Field Name`)
+        .setDesc(
+          `Used to find ${field} set in note's frontmatter/dataview fields`,
+        )
+        .addText((text) => {
+          const save = debounce(
+            async (value: string) => {
+              settings.fieldNames[field] = value;
+              await this.plugin.saveSettings();
+            },
+            500,
+            true,
+          );
+          text
+            .setValue(settings.fieldNames[field])
+            .onChange(async (value: string) => save(value));
+        });
+    setup("parents");
+    setup("children");
   }
 }
